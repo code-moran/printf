9436d1eee4b66b3d3f195eac17a31e17ae8540d8
Titus Njiru <titokym96@gmail.com>
2023-11-13 16:20:02 -0800

    use if

--- _printf.c
+++ _printf.c
@@ -2,18 +2,6 @@
 #include <stdarg.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
-
-/**
  * _printf - prints output according to a format
  * @format: character string containing directives
  *
